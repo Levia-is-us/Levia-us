ee2858ed4bd695b24ed6b5ab6c0eff1d93920569
panda <hi@levia.us>
2025-02-10 10:59:41 +0800

    add env for llm model selection

--- engine/flow/evaluator/evaluator_docgen_flow.py
+++ engine/flow/evaluator/evaluator_docgen_flow.py
@@ -2,15 +2,19 @@ from engine.flow.evaluator.evaluator_docgen_prompt import (
     create_evaluator_docgen_prompt,
 )
 from engine.llm_provider.llm import chat_completion
-import json
+import os
+
+QUALITY_MODEL_NAME = os.getenv("QUALITY_MODEL_NAME")
+FAST_MODEL_NAME = os.getenv("FAST_MODEL_NAME")
 
 
 def evaluator_docgen_flow(code):
     prompt = create_evaluator_docgen_prompt(code)
     result = chat_completion(
-        prompt, model="o1-mini", config={"temperature": 0, "max_tokens": 4000}
+        prompt, model=QUALITY_MODEL_NAME, config={"temperature": 0, "max_tokens": 4000}
     )
     return result
+
 
 def extract_code_breakdown_from_doc(doc):
     start = doc.find("<code_breakdown>") + len("<code_breakdown>")
--- engine/flow/tool_selector/tool_select.py
+++ engine/flow/tool_selector/tool_select.py
@@ -1,8 +1,14 @@
 from engine.flow.tool_selector.tool_select_prompt import tool_selector_prompt
 from engine.llm_provider.llm import chat_completion
-import json
+import os
+
+QUALITY_MODEL_NAME = os.getenv("QUALITY_MODEL_NAME")
+FAST_MODEL_NAME = os.getenv("FAST_MODEL_NAME")
+
 
 def tool_select(plan, current_step, context, tools):
     prompt = tool_selector_prompt(plan, current_step, context, tools)
-    result = chat_completion(prompt, model="o1-mini", config={"temperature": 0, "max_tokens": 4000})
+    result = chat_completion(
+        prompt, model=QUALITY_MODEL_NAME, config={"temperature": 0, "max_tokens": 4000}
+    )
     return result
