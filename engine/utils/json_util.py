6a48bb4925c7107fcba08b5a0920900ce869d944
Oliver <OliverKState@outlook.com>
2025-02-19 11:19:33 +0800

    improve LLM system and updated tools

--- engine/utils/json_util.py
+++ engine/utils/json_util.py
@@ -7,7 +7,10 @@ def extract_json_from_str(json_str):
             start = json_str.find("```json") + len("```json")
             end = json_str.find("```", start)
             json_str = json_str[start:end].strip()
-            return json.loads(json_str)
+            try:
+                return json.loads(json_str)
+            except:
+                return eval(json_str)
         elif ">" in json_str:
             start = json_str.rfind(">") + len(">")
             json_str = json_str[start : len(json_str)].strip()
