e3cd3359ee7da3117c12a4826b2fe3b86b5fffa0
Oliver <OliverKState@outlook.com>
2025-02-25 22:13:42 +0800

    changed upload time file

--- engine/intent_engine/intent_event.py
+++ engine/intent_engine/intent_event.py
@@ -5,15 +5,20 @@ from engine.intent_engine.backup_reply import backup_reply
 from engine.flow.executor.task_manager import TaskManager
 from engine.utils.chat_formatter import create_chat_message
 from metacognitive.stream.stream import output_stream
+import datetime
 short_term_memory = ShortTermMemory()
 
 task_manager = TaskManager()
 
 def event_chat(user_id, input_message):
+    start_time = datetime.datetime.now().timestamp()
+    output_stream(log=f"Start time: {start_time}", user_id=user_id, type="start_time")
     try:
         print("\033[93mWelcome to Levia Chat!\033[0m")
         output_stream(input_message, user_id, "newtask")
         reply = handle_chat_flow(input_message, user_id)
+        end_time = datetime.datetime.now().timestamp()
+        output_stream(log=f"End time: {end_time}", user_id=user_id, type="end_time")
         return reply
     except Exception as e:
         print(f"event_chat error: {str(e)}")
@@ -21,4 +26,6 @@ def event_chat(user_id, input_message):
         short_term_memory.add_context(
             create_chat_message("assistant", f"{reply}"), user_id
         )
+        end_time = datetime.datetime.now().timestamp()
+        output_stream(log=f"End time: {end_time}", user_id=user_id, type="end_time")
         return reply
